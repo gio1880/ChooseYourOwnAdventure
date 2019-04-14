8d713a33d44c7e4c4fa21569befb002dab423db3
Giovanny De Los Angeles <giovannydelosangeles@gmail.com>
2019-04-14 19:49:04 +0000

    Your message here

--- js/game.js
+++ js/game.js
@@ -2,43 +2,94 @@
 
 var game = {
     music: "98_Lost_Mine.mp3",
-    background_image: "intro-bg.jpg",
+    background_image: "cabin.jpg",
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You're on a trip with your friends. On your way there you see a dark figure on the side of the road but don't don't think anything about it. You arrive at the cabin and as you walk towards the entrance you hear a noise. Do you ...",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Check it out",
+                    nextLevel: "NoteOne",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Leave it alone",
+                    nextLevel: "Cabin",
                 },
             ]
         },
-
-        cave: {
-            background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+        
+        Cabin: {
+            background_image: "room.jpeg",
+            music: "",
+            message: "You go inside the cabin and finish unpacking. Do you..",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Explore the house ",
+                    nextLevel: "houseExploration",
                 },
+                {
+                    text: "Go to sleep ",
+                    nextLevel: "LateNight",
+                }
+            ]
+        },
+        
+        NoteOne: {
+            background_image: "bush.jpeg",
+            music: "bushes.mp3",
+            message: "A bunny hops out and half of a note is left behind the note. The note reads: in the house cautious what it seems. You put the note away and go inside the cabin.Do you..",
+            choices: [
+                {
+                    text: "Explore the house ",
+                    nextLevel: "houseExploration",
+                },
+                {
+                    text: "Go to sleep ",
+                    nextLevel: "LateNight",
+                }
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
+        houseExploration: {
+            background_image:"keepoutdoor..jpg",
+            nusic:"",
+            message: "As you walk by the rooms, one in particular catches your eye. A sign on the door states: Stay out. Do you...",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "try to go in",
+                    nextLevel: "comingsoon",
                 },
+                {
+                    text: "respect the owners sign and leave it alone",
+                    nextLevel: "LateNight",
+                },
+            ]
+        },
+        
+        LateNight: {
+            background_image: "",
+            music: "",
+            message: "Its 4:00 am and you hear something coming from the kitchen. Do you ?",
+            choices: [
+                {
+                    text: "look for where the noise is coming from",
+                    nextLevel: "comming soom",
+                },
+                {
+                    text: "Go back to sleep ",
+                    nextLevel: "comingsoon",
+                }
+            ]
+        },
+        
+        comingsoon: {
+            background_image: "",
+            music: "",
+            message: "To be Continued...",
+            choices: [
+                
             ]
         },
 
